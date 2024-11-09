cc8bed8bb3faa7f94c8372926f6115f33e76e6a2
wongcolin45 <wongcolin45@gmail.com>
2024-11-09 18:54:28 -0500

    test update

--- KeyChain/tests/KeyIssuance2.test.ts
+++ KeyChain/tests/KeyIssuance2.test.ts
@@ -1,21 +1,43 @@
 
 import { describe, expect, it } from "vitest";
 
+import { principalCV, cvToJSON , uintCV} from '@stacks/transactions';
+
+
 const accounts = simnet.getAccounts();
 const address1 = accounts.get("wallet_1")!;
 
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
+describe("Key Issuance 2 test", () => {
+  const recipient = principalCV('STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6');
 
-describe("example tests", () => {
-  it("ensures simnet is well initalised", () => {
-    expect(simnet.blockHeight).toBeDefined();
+  it("check contract deployed test", () => {
+    const contractSource = simnet.getContractSource("KeyIssuance2");
+    expect(contractSource).toBeDefined();
   });
 
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
+  it ("issue-key test", () => {
+    const {result} = simnet.callPublicFn(
+      "KeyIssuance2",    // Contract name
+      "issue-key",       // Function name
+      [recipient],      // Arguments for the function (recipient)
+      address1          // Sender address (contract owner in this case)
+    );
+    const data = cvToJSON(result);
+    const tokenID = data.value.value;
+    expect(tokenID).toBe("1");           
+  })
+
+  it ("get-key-details test", () => {
+    const {result} = simnet.callReadOnlyFn(
+      "KeyIssuance2",     // Contract name
+      "get-key-details",  // Function name
+      [uintCV("2")],      // Arguments for the function (recipient)
+      address1            // Sender address (contract owner in this case)
+    );
+    const data = cvToJSON(result);
+    const sucess = data.success;
+    expect(sucess).toBe(true);
+    //expect(cvToJSON(result)).toBe(true);
+  })
+
 });
