91f4ae282e32511da62ebe3f7f98c8ca8db266ef
wongcolin45 <wongcolin45@gmail.com>
2024-11-10 05:57:48 -0500

    fixed contract deploy

--- stacks-frontend/src/contractDeploy.js
+++ stacks-frontend/src/contractDeploy.js
@@ -1,25 +1,31 @@
 import { makeContractDeploy, broadcastTransaction } from '@stacks/transactions';
-import { StacksTestnet } from '@stacks/network'
-import fs from 'fs';
+//import { StacksTestnet } from '@stacks/network'
 
-const network = new StacksTestnet();
-const clarityCode = fs.readFileSync('./KeyIssuance2.clar', 'utf-8');
+import fs from 'fs';
+import pkg from '@stacks/network';
+const { StacksTestnet } = pkg;
+
+const network = StacksTestnet;
+const clarityCode = fs.readFileSync('../../KeyChain/contracts/KeyIssuance2.clar', 'utf-8');
+
 
 async function deployContract(){
     const senderKey = 'user-private-key';
     const txOptions = {
         contractName: 'your-contract-name',
-        codyBody: clarityCode,
-        senderKey: senderKedr,
+        codeBody: clarityCode,
+        senderKey: senderKey,
         network: network,
     };
 
     try {
         const transaction = await makeContractDeploy(txOptions);
         const response = await broadcastTransaction(transaction, network);
-        console.log('Trasanction ID:', responsetxid);
+        console.log('Trasanction ID:', response.txid);
         console.log('Contract Addresses:', response.contract_address)
     } catch (error) {
         
     }
 }
+
+await deployContract()
